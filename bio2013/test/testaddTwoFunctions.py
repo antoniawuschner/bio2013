0d94de76f3178642cd4736c5a38b0b4d436a365b
Antonia Wuschner <Antonia@Antonias-MacBook-Pro.local>
2013-07-05 10:37:27 -0400

    fix the test

--- bio2013/test/testaddTwoFunctions.py
+++ bio2013/test/testaddTwoFunctions.py
@@ -1,7 +1,8 @@
 from .. import addTwoFunctions as a
 import unittest
+import math
 
 class TestFirst(unittest.TestCase):
      
     def testaddTwoFunctions(self):
-        self.assertEqual(a.addTwoFunctions(math.sin,math.cos,0,1),[1,1.017])
+        self.assertEqual(a.addTwoFunctions(math.sin,math.cos,0,2),[1.0,1.3817732906760363])
