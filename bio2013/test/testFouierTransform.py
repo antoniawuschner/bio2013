b3b0404170b3d8d82ec0fddef01c5e4df2badb55
Antonia Wuschner <Antonia@Antonias-MacBook-Pro.local>
2013-07-09 14:24:48 -0400

    add fourier test

---
+++ bio2013/test/testFouierTransform.py
@@ -0,0 +1,13 @@
+from .. finalModules import CalculateFT as Fourier
+import unittest
+import math
+import numpy as np
+
+class TestFirst(unittest.TestCase):
+     
+    def testFourierTransform(self):
+        a = [ 6.0+0.j,-1.5+0.8660254j,-1.5-0.8660254j]
+        b = Fourier.FourierSequence([1,2,3])
+        for y in range(0,len(a)):
+            self.assertAlmostEqual(a[y],b[y])
+        
